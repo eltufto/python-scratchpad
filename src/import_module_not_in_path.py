6fbc5cd9b43a62e6d8b4f7032f97cf0c546e0f7c
Stephen Tufts <stephen@oak.(none)>
2013-01-17 10:50:02 -0800

    Updating some comments

--- src/import_module_not_in_path.py
+++ src/import_module_not_in_path.py
@@ -3,7 +3,6 @@ import imp
 from xdg.BaseDirectory import xdg_cache_home
 CACHE_PATH = os.path.join(xdg_cache_home, 'scratchpad')
 standalone_file = 'import_from_cache'
-directory_test_file = "sub_dir.catalina_test"
 
 
 import sys
@@ -14,12 +13,4 @@ attempt_4 = os.path.abspath(os.path.join('/some/path/', os.path.pardir))
 #sys.path.append(to_add_to_sys_path)
 sys.path.append(xdg_cache_home)
 scratchpad_module = __import__('scratchpad')
-#test_definition = __import__(standalone_file)
-#test_definition.run()
-#print("SJT - importing test %s" % directory_test_file)
-#try:
-#    test_definition = __import__(directory_test_file)
-#except ImportError:
-#    print("SJT - Unknown test %s" % directory_test_file)
-#test_definition.run()
 print scratchpad_module
--- src/named_tuples.py
+++ src/named_tuples.py
@@ -5,11 +5,15 @@ class SomeClass(object):
         self.data2 = data2
     def __str__(self):
         return self.data1 + " " + self.data2
-        
-MyNamedTuple = namedtuple('MyNamedTuple','some_class, some_var1, some_var2')
+
 some_class = SomeClass('aa','bb')
+
+#define the fields        
+MyNamedTuple = namedtuple('MyNamedTuple','some_class, some_var1, some_var2')
+#create an instance
 mnt = MyNamedTuple(some_class, 1, "hello")
 
+#access fields
 print str(mnt.some_class)
 print mnt.some_var1
 print mnt.some_var2
