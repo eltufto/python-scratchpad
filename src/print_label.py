57f013f1c9e0c0787c9543cea719a9213d4b2b23
Stephen Tufts <stephen@oak.(none)>
2013-01-22 18:24:58 -0800

    dbus tests

--- src/print_label.py
+++ src/print_label.py
@@ -9,8 +9,9 @@ def print_label(result_dir, info):
             print_job.write('%s: %s\n' % (key, info[key]))
 
     print "Label is printing ..."
-    #cmd = ('lp -d %s -o landscape -o PageSize=w79h252.2 -o number-up=2 %s' %
-    cmd = ('lp -d %s -o landscape -o fit-to-page -o media=Custom.18x36 -o number-up=2 %s' %
+    cmd = ('lp -d %s -o landscape -o PageSize=w79h252.2 -o number-up=2 %s' %
+    #cmd = ('lp -d %s -o landscape -o fit-to-page -o media=Custom.18x36 -o number-up=2 %s' %
+    #cmd = ('lp -d %s -o landscape -o number-up=2 %s' %
             (printer_name, os.path.join(print_dir, 'print.txt')))
     os.system(cmd)
 
